0c5c1e22ebdde70c194f429d8425c1bf9ee2adc1
allexib <ooomegasib@gmail.com>
2020-12-06 17:13:11 +0400

    29 data to state.first step to redux

---
+++ react-kabzda-1/src/redux/state.js
@@ -0,0 +1,28 @@
+
+let state = {
+    profilePage: {
+        posts: [
+            {id: 1, message: 'hi nigga how a y', likesCount: 18},
+            {id: 2, message: 'hi nigga how a y', likesCount: 16},
+            {id: 3, message: 'qq niga', likesCount: 12},
+            {id: 4, message: 'it is my first post', likesCount: 11}
+        ]
+
+    },
+    dialogsPage:{
+        dialogs: [
+            {id: 1, name: 'Dima'},
+            {id: 2, name: 'Andrey'},
+            {id: 3, name: 'Sveta'},
+            {id: 4, name: 'Viktor'},
+            {id: 5, name: 'Valera'}
+        ],
+        messages: [
+            {id: 1, message: 'Hi'},
+            {id: 2, message: 'How is your progress'},
+            {id: 3, message: 'Yo'}
+        ]
+    }
+}
+
+export default state;
