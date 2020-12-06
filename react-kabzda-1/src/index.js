0d4f99834a7a8a926a027af43c5fb034d05bb8d0
allexib <ooomegasib@gmail.com>
2020-12-06 17:12:02 +0400

    29 data to state

--- react-kabzda-1/src/index.js
+++ react-kabzda-1/src/index.js
@@ -3,32 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-
-let posts = [
-    {id: 1, message: 'hi nigga how a y', likesCount: 18},
-    {id: 2, message: 'hi nigga how a y', likesCount: 16},
-    {id: 3, message: 'qq niga', likesCount: 12},
-    {id: 4, message: 'it is my first post', likesCount: 11}
-]
-
-let dialogs = [
-    {id: 1, name: 'Dima'},
-    {id: 2, name: 'Andrey'},
-    {id: 3, name: 'Sveta'},
-    {id: 4, name: 'Viktor'},
-    {id: 5, name: 'Valera'}
-]
-
-let messages = [
-    {id: 1, message: 'Hi'},
-    {id: 2, message: 'How is your progress'},
-    {id: 3, message: 'Yo'}
-]
+import state from './redux/state';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App posts={posts} dialogs={dialogs} messages={messages}/>
+    <App state={state} />
   </React.StrictMode>,
   document.getElementById('root')
 );
