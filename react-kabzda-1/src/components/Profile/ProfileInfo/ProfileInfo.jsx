6d7f9a51438b1deb713262dd9cbb5d489c6faa04
allexib <ooomegasib@gmail.com>
2020-12-24 17:04:59 +0400

    59 profile page, ajax, api

--- react-kabzda-1/src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ react-kabzda-1/src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import s from './ProfileInfo.module.css';
+import Preloader from "../../common/Preloader/Preloader";
 
 
 
-const ProfileInfo = () => {
+const ProfileInfo = (props) => {
+    if (!props.profile) {
+        return <Preloader />
+    }
+
     return (
         <div>
             <div>
@@ -11,6 +16,7 @@ const ProfileInfo = () => {
                     src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQE_uWpeaS6wOoCym0mv8IUPjvLWD_XxMoq0Q&usqp=CAU'/>
             </div>
             <div className={s.descriptionBlock}>
+                <img src={props.profile.photos.large} />
                 ava+description
             </div>
 
--- react-kabzda-1/src/components/Users/Users.jsx
+++ react-kabzda-1/src/components/Users/Users.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './users.module.css';
 import userPhoto from '../../../src/assets/images/user.jpg';
+import {NavLink} from "react-router-dom";
 
 let Users = (props) => {
 
@@ -22,8 +23,10 @@ let Users = (props) => {
             props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
+                        <NavLink to={'/profile/' + u.id}>
                         <img src={u.photos.small != null ? u.photos.small : userPhoto}
                              className={styles.userPhoto}/>
+                        </NavLink>
                     </div>
                     <div>
                         {u.followed
