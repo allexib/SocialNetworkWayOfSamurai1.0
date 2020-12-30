512818f2c4b251ad6328c1a97ce6a31feae3a90f
allexib <ooomegasib@gmail.com>
2020-12-30 14:34:47 +0400

    68 redirect

--- react-kabzda-1/src/api/api.js
+++ react-kabzda-1/src/api/api.js
@@ -1,6 +1,6 @@
 import * as axios from "axios";
 
-const instance = axios.create ({
+const instance = axios.create({
     withCredentials: true,
     baseURL: 'https://social-network.samuraijs.com/api/1.0/',
     headers: {
@@ -10,16 +10,25 @@ const instance = axios.create ({
 
 export const usersAPI = {
     getUsers(currentPage = 1, pageSize = 10) {
-        return instance.get(`users?page=${currentPage}&count=${pageSize}`, )
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`,)
             .then(response => {
                 return response.data;
             });
     },
     follow(userId) {
-        return instance.post(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`)
+        return instance.post(`follow/${userId}`)
     },
     unfollow(userId) {
-        return instance.delete(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`)
+        return instance.delete(`follow/${userId}`)
+    },
+    getProfile(userId) {
+        return instance.get(`profile/` + userId);
+    }
+}
+
+export const authAPI = {
+    me() {
+       return instance.get(`auth/me`)
     }
 }
 
