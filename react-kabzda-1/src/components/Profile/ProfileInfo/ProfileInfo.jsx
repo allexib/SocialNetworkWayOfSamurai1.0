07947ff4374228b5c6e85df518e44b35ad1accd4
allexib <ooomegasib@gmail.com>
2020-12-31 19:17:43 +0400

    73 http put, local state

--- react-kabzda-1/src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ react-kabzda-1/src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -18,7 +18,7 @@ const ProfileInfo = (props) => {
             {/*</div>*/}
             <div className={s.descriptionBlock}>
                 <img src={props.profile.photos.large} />
-                <ProfileStatus status={"test niggg"}/>
+                <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
             </div>
 
         </div>
--- react-kabzda-1/src/redux/profile-reducer.js
+++ react-kabzda-1/src/redux/profile-reducer.js
@@ -1,19 +1,21 @@
-import {usersAPI} from "../api/api";
+import {profileAPI, usersAPI} from "../api/api";
 
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 const SET_USER_PROFILE = 'SET_USER_PROFILE';
+const SET_STATUS = 'SET_STATUS';
 
 let initialState = {
-        posts: [
-            {id: 1, message: 'hi nigga how a y', likesCount: 18},
-            {id: 2, message: 'hi nigga how a y', likesCount: 16},
-            {id: 3, message: 'qq niga', likesCount: 12},
-            {id: 4, message: 'it is my first post', likesCount: 11}
-        ],
-        newPostText: 'it-kama',
-        profile: null
-    };
+    posts: [
+        {id: 1, message: 'hi nigga how a y', likesCount: 18},
+        {id: 2, message: 'hi nigga how a y', likesCount: 16},
+        {id: 3, message: 'qq niga', likesCount: 12},
+        {id: 4, message: 'it is my first post', likesCount: 11}
+    ],
+    newPostText: 'it-kama',
+    profile: null,
+    status: ""
+};
 
 const profileReducer = (state = initialState, action) => {
     switch (action.type) {
@@ -35,6 +37,12 @@ const profileReducer = (state = initialState, action) => {
                 newPostText: action.newText
             };
         }
+        case SET_STATUS: {
+            return {
+                ...state,
+                status: action.status
+            };
+        }
         case SET_USER_PROFILE: {
             return {...state, profile: action.profile}
         }
@@ -45,12 +53,30 @@ const profileReducer = (state = initialState, action) => {
 
 export const addPostActionCreator = () => ({type: ADD_POST})
 export const setUserProfile = (profile) => ({type: SET_USER_PROFILE, profile})
+export const setStatus = (status) => ({type: SET_STATUS, status})
 
 export const getUserProfile = (userId) => (dispatch) => {
     usersAPI.getProfile(userId).then(response => {
-        dispatch (setUserProfile(response.data));
+        dispatch(setUserProfile(response.data));
     });
 }
+export const getStatus = (userId) => (dispatch) => {
+    profileAPI.getStatus(userId)
+        .then(response => {
+
+            dispatch(setStatus(response.data));
+        });
+}
+
+export const updateStatus = (status) => (dispatch) => {
+    profileAPI.updateStatus(status)
+        .then(response => {
+            if (response.data.resultCode === 0) {
+                dispatch(setStatus(status));
+            }
+        });
+}
+
 
 export const updateNewPostTextActionCreator = (text) =>
     ({type: UPDATE_NEW_POST_TEXT, newText: text})
