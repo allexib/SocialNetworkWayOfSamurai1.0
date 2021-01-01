1f193d947ec6258312eb9b8d83eca63d06e0986a
allexib <ooomegasib@gmail.com>
2021-01-01 22:33:35 +0400

    76 redux-form, basic

--- react-kabzda-1/src/redux/dialogs-reducer.js
+++ react-kabzda-1/src/redux/dialogs-reducer.js
@@ -1,5 +1,4 @@
-const UPDATE_NEW_MESSAGE_BODY = 'UPDATE_NEW_MESSAGE_BODY';
-const SEND_MESSAGE = 'SEND_MESSAGE';
+ const SEND_MESSAGE = 'SEND_MESSAGE';
 
 let initialState = {
     dialogs: [
@@ -13,24 +12,16 @@ let initialState = {
         {id: 1, message: 'Hi'},
         {id: 2, message: 'How is your progress'},
         {id: 3, message: 'Yo'}
-    ],
-    newMessageBody: ""
+    ]
 };
 
 
 const dialogsReducer = (state = initialState, action) => {
     switch (action.type) {
-        case UPDATE_NEW_MESSAGE_BODY:
-            return {
-                ...state,
-                newMessageBody: action.body
-            };
-
         case SEND_MESSAGE:
-            let body = state.newMessageBody;
+            let body = action.newMessageBody;
             return {
                 ...state,
-                newMessageBody: '',
                 messages: [...state.messages, {id: 4, message: body}]
             };
         default:
@@ -38,8 +29,7 @@ const dialogsReducer = (state = initialState, action) => {
     }
 }
 
-export const sendMessageCreator = () => ({type: SEND_MESSAGE})
-export const updateNewMessageBodyCreator = (body) =>
-    ({type: UPDATE_NEW_MESSAGE_BODY, body: body})
+export const sendMessageCreator = (newMessageBody) => ({type: SEND_MESSAGE, newMessageBody})
+
 
 export default dialogsReducer;
