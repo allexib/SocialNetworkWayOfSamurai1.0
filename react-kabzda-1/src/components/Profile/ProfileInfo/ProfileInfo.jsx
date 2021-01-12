186974273a586153813acf78e403f4b25e12609d
allexib <ooomegasib@gmail.com>
2021-01-12 15:49:09 +0400

    90 redux-ducks, refactoring

--- react-kabzda-1/src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ react-kabzda-1/src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -5,21 +5,16 @@ import ProfileStatus from "./ProfileStatus";
 import ProfileStatusWithHooks from "./ProfileStatusЦWithHooks";
 
 
-
-const ProfileInfo = (props) => {
-    if (!props.profile) {
-        return <Preloader />
+const ProfileInfo = ({profile, status, updateStatus}) => {
+    if (!profile) {
+        return <Preloader/>
     }
 
     return (
         <div>
-            {/*<div>*/}
-                {/*<img*/}
-                    {/*src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQE_uWpeaS6wOoCym0mv8IUPjvLWD_XxMoq0Q&usqp=CAU'/>*/}
-            {/*</div>*/}
             <div className={s.descriptionBlock}>
-                <img src={props.profile.photos.large} />
-                <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
+                <img src={profile.photos.large}/>
+                <ProfileStatusWithHooks status={status} updateStatus={updateStatus}/>
             </div>
 
         </div>
