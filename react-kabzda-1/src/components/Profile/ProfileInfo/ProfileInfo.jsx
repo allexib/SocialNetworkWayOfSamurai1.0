2762bb44d8a0d36b7b19a3a8c05e88aa735fa2c5
allexib <ooomegasib@gmail.com>
2021-01-15 19:20:30 +0400

    96.5 ava loading

--- react-kabzda-1/src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ react-kabzda-1/src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,24 +1,58 @@
 import React from 'react';
 import s from './ProfileInfo.module.css';
 import Preloader from "../../common/Preloader/Preloader";
-import ProfileStatus from "./ProfileStatus";
 import ProfileStatusWithHooks from "./ProfileStatusЦWithHooks";
+import userPhoto from '../../../assets/images/user.jpg';
 
-
-const ProfileInfo = ({profile, status, updateStatus}) => {
+const ProfileInfo = ({profile, status, updateStatus, isOwner, savePhoto}) => {
     if (!profile) {
         return <Preloader/>
+    }
+
+    const onMainPhotoSelected = (e) => {
+        if (e.target.files.length) {
+            savePhoto(e.target.files[0]);
+        }
     }
 
     return (
         <div>
             <div className={s.descriptionBlock}>
-                <img src={profile.photos.large}/>
+                <img src={profile.photos.large || userPhoto} className={s.mainPhoto}/>
+                {isOwner && <input type={"file"} onChange={onMainPhotoSelected}/>}
+
+                <div>
+                    <div>
+                        <b>Full name</b>: {profile.fullName}
+                    </div>
+                    <div>
+                        <b>Looking for a job</b>: {profile.lookingForAJob ? "yes" : "no"}
+                    </div>
+                    {profile.lookingForAJob &&
+                    <div>
+                        <b>My professional skills</b>: {profile.lookingForAJobDescription}
+                    </div>
+                    }
+                    <div>
+                        <b>About me</b>: {profile.aboutMe}
+                    </div>
+                    <div>
+                        <b>Contacts</b>: {Object.keys(profile.contacts).map(key => {
+                        return <Contact key={key} contactTitle={key} contactValue={profile.contacts[key]}/>
+                    })}
+                    </div>
+                </div>
+
                 <ProfileStatusWithHooks status={status} updateStatus={updateStatus}/>
             </div>
 
         </div>
     )
 }
+
+const Contact = ({contactTitle, contactValue}) => {
+    return <div className={s.contact}><b>{contactTitle}</b>: {contactValue}</div>
+}
+
 
 export default ProfileInfo;
